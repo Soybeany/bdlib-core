89092da9251e98096f2578e1b9a5617fa031b4fe
Soybeany <soybeany@qq.com>
2019-06-13 11:39:38 +0800

    [~] GsonParser引入Json观察者的设计，便于输出日志或其它操作

--- web_okhttp/src/main/java/com/soybeany/bdlib/web/okhttp/parser/GsonParser.java
+++ web_okhttp/src/main/java/com/soybeany/bdlib/web/okhttp/parser/GsonParser.java
@@ -2,7 +2,12 @@ package com.soybeany.bdlib.web.okhttp.parser;
 
 import com.google.gson.Gson;
 import com.soybeany.bdlib.core.java8.Optional;
+import com.soybeany.bdlib.core.java8.function.Consumer;
+import com.soybeany.bdlib.core.util.IterableUtils;
 import com.soybeany.bdlib.core.util.file.IProgressListener;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import okhttp3.ResponseBody;
 
@@ -13,6 +18,7 @@ public abstract class GsonParser<T> implements IParser<T> {
     private static final Gson DEFAULT_GSON = new Gson();
 
     private Gson mGson = DEFAULT_GSON;
+    private final Set<Consumer<String>> mJsonWatchers = new HashSet<>();
 
     public GsonParser<T> gson(Gson gson) {
         Optional.ofNullable(gson).ifPresent(g -> mGson = g);
@@ -21,7 +27,24 @@ public abstract class GsonParser<T> implements IParser<T> {
 
     @Override
     public T parse(ResponseBody body, IProgressListener listener) throws Exception {
-        return onParse(mGson, StringParser.get().parse(body, null));
+        String json = StringParser.get().parse(body, listener);
+        IterableUtils.forEach(mJsonWatchers, (watcher, flag) -> watcher.accept(json));
+        return onParse(mGson, json);
+    }
+
+    /**
+     * 添加Json观察者，观察解析后的json
+     */
+    public GsonParser<T> addJsonWatcher(Consumer<String> watcher) {
+        mJsonWatchers.add(watcher);
+        return this;
+    }
+
+    /**
+     * 获得全部Json观察者
+     */
+    public Set<Consumer<String>> jsonWatchers() {
+        return mJsonWatchers;
     }
 
     protected abstract T onParse(Gson gson, String json) throws Exception;
