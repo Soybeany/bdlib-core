4f955796b2bac4074c8f70e69d7db00131b4eccd
Soybeany <soybeany@qq.com>
2020-04-01 23:48:28 +0800

    [-] 移除日志提取相关的类

--- log_extract/src/main/java/com/soybeany/bdlib/log/extract/efb/UserFilter.java
+++
@@ -1,7 +0,0 @@
-package com.soybeany.bdlib.log.extract.efb;
-
-/**
- * <br>Created by Soybeany on 2019/5/31.
- */
-public class UserFilter {
-}
--- log_extract/src/main/java/com/soybeany/bdlib/log/extract/model/IFlagItem.java
+++
@@ -1,7 +0,0 @@
-package com.soybeany.bdlib.log.extract.model;
-
-/**
- * <br>Created by Soybeany on 2019/6/10.
- */
-public interface IFlagItem {
-}
